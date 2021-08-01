e46973da3322c3c9399db60fda4de225fd348194
AniElaina <anicook13@gmail.com>
2021-08-01 09:49:43 -0400

    fixed error

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-/////////////////// SET UP DEPENDENCIES & BOILERPLATE ////////////////////////////
+// SET UP DEPENDENCIES & BOILERPLATE //
 const fs = require("fs");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//// IMPORT THE "PATH" module here ////
+// IMPORT THE "PATH" module here //
 const path = require("path");
-//// SET VARIABLE THAT REQUIRES DB FILE TO ACCESS DATA //// 
+//// SET VARIABLE THAT REQUIRES DB FILE TO ACCESS DATA //
 const userNotes = require("./db/db.json")
 
 //// set up Express app to handle data parsing ////
-app.use(express.urlencoded({ extended: true })); //EXPRESS.JS METHOD THAT TAKES INCOMING POST DATA AND CONVERTS TO KEY/VALUE PARINGS THAT CAN BE ACCESSED IN THE REQ.BODY OBJECT   
-app.use(express.json());   /// EXPRESS.JS METHOD THAT TAKES INNCOMING POST DATA AND PARSES INTO THE REQ.BODY 
-app.use(express.static("public")); // USE THE EXPRESS.STATIC MIDDLEWARE FUNCTION TO ACCESS PUBLIC DIRECTORY 
-
-////////////////// END BOILERPLATE & DEPENDENCIES ////////////////////////////////////////////
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// END BOILERPLATE & DEPENDENCIES //
 
 /// ROUTES START HERE ///////////////
-/// HOME PAGE /////
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -37,23 +35,28 @@ app.get("/api/notes", (req, res) => {
 });
 
 
-
-
-///// POST /API/NOTES SHOULD RECEIVE A NEW NOTE TO SAVE ON THE REQUEST BODY AND ADD IT TO DB.JSON FILE, & RETURN NEW NOTE TO CLIENT 
+// POST /API/NOTES SHOULD RECEIVE A NEW NOTE TO SAVE ON THE REQUEST BODY AND ADD IT TO DB.JSON FILE, & RETURN NEW NOTE TO CLIENT 
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
+    newNote.id = (userNotes.length).toString();
 
     userNotes.push(newNote);
+
+    // WRITE FILE, STRINGIFY USERNOTES ARRAY, AND LOG ERROR IF THERE 
+    fs.writeFile(notes, JSON.stringify(userNotes), (err) => {
+        if (err)
+            console.log(err);
+    })
     res.json(newNote);
 })
 
-/// GET * SHOULD BE USED HERE TO RETURN THE INDEX.HTML FILE 
-/// THE * ROUTE WILL RETURN USER TO HOME PAGE AND SHOULD ALWAYS COME AFTER OTHER ROUTES THAT 'GET' OR IT WILL TAKE PRECEDENCE OVER THE OTHERS. ///// 
+
+// THE * ROUTE WILL RETURN USER TO HOME PAGE AND SHOULD ALWAYS COME AFTER OTHER ROUTES THAT 'GET' OR IT WILL TAKE PRECEDENCE OVER THE OTHERS. // 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
-///// APP IS LISTENING TO PORT //////////////
+// APP IS LISTENING TO PORT //
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 })
